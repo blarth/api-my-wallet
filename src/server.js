e4bfb581ce37789c4e777bfc302bc2bf898a5823
blarth <jminocente@gmail.com>
2022-02-01 23:55:43 -0300

    feat: sign-up listener

--- src/server.js
+++ src/server.js
@@ -5,6 +5,8 @@ import express, { json } from "express";
 import joi from "joi";
 import dotenv from "dotenv";
 import dayjs from "dayjs";
+import bcrypt from 'bcrypt'
+import {v4 as tokenGenerator} from "uuid"
 
 dotenv.config();
 
@@ -18,26 +20,56 @@ const server = express();
 server.use(cors());
 server.use(json());
 
+function sanitizeData(string) {
+    return stripHtml(string.trim()).result;
+  }
 
-app.post("/sign-up", async (req, res) => {
-  //name, email, password
-  const user = req.body;
+const schemaSignUp = joi.object({
+    name: joi.string().required(),
+    email: joi.string().email().required(),
+    password: joi.string().required(),
+  });
+const schemaLogin = joi.object({
+    name: joi.string().min(1).required(),
+  });
+
+server.post("/sign-up", async (req, res) => {
   
+  const user = req.body;
 
-  const passwordHash = bcrypt.hashSync(user.password, 10);
+  const validation = schemaSignUp.validate(user)
 
-  await db.collection("users").insertOne({ ...user, password: passwordHash });
+  if (validation.error) {
+    res.status(422).send("Name deve ser strings não vazio");
+    console.log(validation.error.details);
+    return;
+  }
 
-  res.sendStatus(201);
+  const userData = {
+      name : sanitizeData(user.name),
+      email : sanitizeData(user.email),
+      password : sanitizeData(user.password)
+  }
+
+  const passwordHash = bcrypt.hashSync(userData.password, parseInt(process.env.DIF));
+  try {
+      await db.collection("users").insertOne({ ...userData, password: passwordHash });
+      res.sendStatus(201);
+  } catch (error) {
+      console.log(error)
+      res.sendStatus(500)
+  }
+
+  
 });
 
-app.post("/sign-in", async (req, res) => {
+server.post("/login", async (req, res) => {
   const { email, password } = req.body;
 
   const user = await db.collection("users").findOne({ email });
 
   if (user && bcrypt.compareSync(password, user.password)) {
-    const token = uuid();
+    const token = tokenGenerator();
 
     await db.collection("sessions").insertOne({
       userId: user._id,
@@ -50,7 +82,7 @@ app.post("/sign-in", async (req, res) => {
   }
 });
 
-app.listen(5000, () => {
+server.listen(5000, () => {
   console.log("Server is listening on port 5000.");
 });
 
