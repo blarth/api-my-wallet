e663c97837467456754b4453c983feb5885d1f7e
Marcio Cunha <marcioabcd11@gmail.com>
2022-02-14 18:09:14 -0300

    Preparando Review

--- src/Schema/SchemaEntries.js
+++
@@ -1,9 +0,0 @@
-import joi from "joi";
-
-const schemaEntries = joi.object({
-    value: joi.string().required(),
-    description : joi.string().required(),
-    type: joi.string().valid("in", "out").required(),
-  });
-
-export default schemaEntries;
